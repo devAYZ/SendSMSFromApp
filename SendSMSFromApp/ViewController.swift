4b5c9105987057f778f67f2b285f40f961b28d12
devAYZ <afatokimi@getcarbon.co>
2021-10-30 09:09:50 +0100

    Initial Commit

---
+++ SendSMSFromApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SendSMSFromApp
+//
+//  Created by Ayokunle Fatokimi on 30/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
