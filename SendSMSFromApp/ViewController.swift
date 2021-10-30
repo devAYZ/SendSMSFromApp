5cb1e478d1ed613bb9c70878180e667b2680ae24
devAYZ <fatokimiemman@gmail.com>
2021-10-30 09:33:17 +0100

    add message ui to vc

--- SendSMSFromApp/ViewController.swift
+++ SendSMSFromApp/ViewController.swift
@@ -6,18 +6,48 @@
 //
 
 import UIKit
+import MessageUI
 
 class ViewController: UIViewController {
 
+    //MARK: IBOutlets
+    @IBOutlet weak var sendMsgBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
-}
-
-extension ViewController {
     
+    func setupView() {
+        sendMsgBtn.addTarget(self, action: #selector(sendMessage), for: .touchUpInside)
+    }
 }
+
+extension ViewController: MFMessageComposeViewControllerDelegate {
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        controller.dismiss(animated: true)
+    }
+    
+    @objc func sendMessage() {
+        if (MFMessageComposeViewController.canSendText()) {
+            let composerVC = MFMessageComposeViewController()
+            composerVC.messageComposeDelegate = self
+            composerVC.body = ""
+            let number = "234"
+            composerVC.recipients = ["\(number)"]
+            self.present(composerVC, animated: true)
+        } else {
+            
+            /// if device can not send sms
+            
+            /// display alert
+            
+            /// or force display message app
+            let sms: String = "sms:08992342342&body=Hello Abc How are You I am ios developer."
+            let strURL: String = sms.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+            UIApplication.shared.open(URL.init(string: strURL)!, options: [:], completionHandler: nil)
+        }
+    }
+}
+
 
